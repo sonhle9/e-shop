ff0e32c7fd5fb983ceab71a36b6735279251e42b
Jędrek Domański <jedrek.domanski@gmail.com>
2017-01-09 22:42:52 +0100

    Additional validations to handle negative values of new products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product <ApplicationRecord
   has_many :cart_items
-  validates :name, :description, :price, :stock, presence: true
+  validates :name, :description, :image_url, presence: true
+  validates :stock, :price, greater_than_or_equal_to: 0
+
   before_destroy :ensure_not_referenced_by_any_cart_item
 
   def ensure_not_referenced_by_any_cart_item
