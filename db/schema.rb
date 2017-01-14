1a3843358d3a78f3b972182c4b58444356909ba4
Jędrek Domański <jedrek.domanski@gmail.com>
2017-01-14 17:11:40 +0100

    Implement advanced login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170111200336) do
+ActiveRecord::Schema.define(version: 20170114150553) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170111200336) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
